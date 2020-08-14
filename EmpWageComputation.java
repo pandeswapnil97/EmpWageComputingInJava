fcabfef068b693970350bb312290f41b07d3b33c
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-14 20:48:22 +0530

    Adding Part Time Employee and Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -25,8 +25,9 @@ public class EmpWageComputation
 
 
 
-//Use Case 2 (Calculate Daily Employee Wage)
 
+//Use Case 2 (Calculate Daily Employee Wage)
+/*
 public class EmpWageComputation
 {
     public static void main(String[] args)
@@ -45,6 +46,43 @@ public class EmpWageComputation
         }
 
         empWage = empRatePerHr * empHrs;
+        System.out.println("Employee's Daily Wage: " + empWage);
+    }
+}
+*/
+
+
+
+
+
+
+
+//Use Case 3 (Adding Part Time Employee and Wage)
+
+public class EmpWageComputation 
+{
+    public static void main(String[] args)
+    {
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
+        int empRatePerHr = 20;
+        
+        int empHrs = 0;
+        int empWage;
+        
+        int empCheck=(int)(Math.floor(Math.random()*10)%3);
+
+        if(IS_FULL_TIME==empCheck)
+        {
+            empHrs = 8;
+        }
+        else if (IS_PART_TIME==empCheck)
+        {
+            empHrs = 4;
+        }
+        
+        empWage = empRatePerHr * empHrs;
+        
         System.out.println("Employee's Daily Wage: " + empWage);
     }
 }
