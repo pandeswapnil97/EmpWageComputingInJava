d15113eaa1a1a7161ca61cefc908f1b57092dd4b
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-14 20:38:21 +0530

    Calculating Daily Employee Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,5 +1,6 @@
-//Use Case 1
+//Use Case 1 (Check whether employee is present or not)
 
+/*
 public class EmpWageComputation
 {
 	public static void main(String[] args)
@@ -18,4 +19,32 @@ public class EmpWageComputation
             		System.out.println("Employee is Absent");
         	}	
 	}
+}
+*/
+
+
+
+
+//Use Case 2 (Calculate Daily Employee Wage)
+
+public class EmpWageComputation
+{
+    public static void main(String[] args)
+    {
+        int IS_PRESENT = 1;
+        int empRatePerHr = 20;
+
+        int empHrs = 0;
+        int empWage;
+
+        int empCheck=(int)(Math.floor(Math.random()*10)%2);
+
+        if(IS_PRESENT==empCheck)
+        {
+            empHrs = 8;
+        }
+
+        empWage = empRatePerHr * empHrs;
+        System.out.println("Employee's Daily Wage: " + empWage);
+    }
 }
