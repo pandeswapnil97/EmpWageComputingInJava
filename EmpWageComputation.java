9ff03ede696242f295acaa5a61eb1f1e6a056212
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-14 20:54:09 +0530

    Solving Using Switch Case

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -58,7 +58,7 @@ public class EmpWageComputation
 
 
 //Use Case 3 (Adding Part Time Employee and Wage)
-
+/*
 public class EmpWageComputation 
 {
     public static void main(String[] args)
@@ -83,6 +83,48 @@ public class EmpWageComputation
         
         empWage = empRatePerHr * empHrs;
         
+        System.out.println("Employee's Daily Wage: " + empWage);
+    }
+}
+*/
+
+
+
+
+
+
+
+//Use Case 4 (Solving Using Switch Case)
+
+public class EmpWageComputation
+{
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_RATE_PER_HR = 20;
+
+    public static void main(String[] args)
+    {
+        int empHrs = 0;
+        int empWage;
+
+        int empCheck=(int)(Math.floor(Math.random()*10)%3);
+
+        switch(empCheck)
+        {
+            case IS_FULL_TIME:
+            {
+                empHrs=8;
+                break;
+            }
+            case IS_PART_TIME:
+            {
+                empHrs=4;
+                break;
+            }
+        }
+
+        empWage = EMP_RATE_PER_HR * empHrs;
+
         System.out.println("Employee's Daily Wage: " + empWage);
     }
 }
