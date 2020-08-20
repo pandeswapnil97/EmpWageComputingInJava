073d3d3b9f337e496eaf12d6bb2e0300374947e9
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-20 22:05:12 +0530

    Calculating Wages for a month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,32 +1,43 @@
-//Use Case 4 (Solving Using Switch Case)
+//Use Case 5 (Calculating Wages for a Month)
 
 public class EmpWageComputation
 {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HR = 20;
+    public static final int NUMBER_OF_WORKING_DAYS = 20;
     
     public static void main(String[] args)
     {
         int empHrs = 0;
-        int empWage;
-        int empCheck = (int)(Math.floor(Math.random()*10)%3);
+        int empDailyWage;
+        int empTotalWage = 0;
+        int totalWorkingDays = 0;
         
-	switch(empCheck)
+	while(totalWorkingDays < NUMBER_OF_WORKING_DAYS)
         {
-            case IS_FULL_TIME:
+            totalWorkingDays = totalWorkingDays + 1;
+            int empCheck = (int)(Math.floor(Math.random()*10)%3);
+            
+	    switch(empCheck)
             {
-                empHrs = 8;
-                break;
+                case IS_FULL_TIME:
+                {
+                    empHrs=8;
+                    break;
+                }
+                case IS_PART_TIME:
+                {
+                    empHrs=4;
+                    break;
+                }
             }
-            case IS_PART_TIME:
-            {
-                empHrs = 4;
-                break;
-            }
-        }
+            
+	    empDailyWage = EMP_RATE_PER_HR * empHrs;
+            empTotalWage = empTotalWage + empDailyWage;
 
-        empWage = EMP_RATE_PER_HR * empHrs;
-        System.out.println("Employee's Daily Wage: " + empWage);
+            System.out.println("Employee's Daily Wage for Day " + totalWorkingDays + ": " + empDailyWage);
+        }
+        System.out.println("Employee's Total Daily Wage for Month: " + empTotalWage);
     }
 }
