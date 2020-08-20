6b1c59869d81c1eec7c2bd2a8aff44a3362a53f1
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-20 21:24:56 +0530

    Checking whether Employee is present or not

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,10 +1,13 @@
-//Use Case 1
+//Use Case 1 (Checking whether Employee is present or not)
 
 public class EmpWageComputation
 {
 	public static void main(String[] args)
 	{
-		//System.out.println("Welcome to Employee Wage Computation Problem in Java");
+		System.out.println("Welcome to Employee Wage Computation Problem in Java");
+		
+		System.out.println("     ");
+
 		int IS_PRESENT=1;
         
         	int empCheck=(int)(Math.floor(Math.random()*10)%2);
