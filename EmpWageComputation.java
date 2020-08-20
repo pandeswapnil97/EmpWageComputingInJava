e9078b1d776fb91e370da47697ec7a8af3b6c6b2
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-20 21:41:19 +0530

    Welcome to Employee Wage Computation Problem in Java

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,27 +1,7 @@
-<<<<<<< HEAD
-=======
-//Use Case 1 (Checking whether Employee is present or not)
-
 public class EmpWageComputation
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Employee Wage Computation Problem in Java");
-		
-		System.out.println("     ");
-
-		int IS_PRESENT=1;
-        
-        	int empCheck=(int)(Math.floor(Math.random()*10)%2);
-          
-        	if(IS_PRESENT==empCheck)
-        	{
-            		System.out.println("Employee is Present");
-       		}
-        	else
-        	{
-            		System.out.println("Employee is Absent");
-        	}	
+		System.out.println("Welcome to Employee Wage Computation Problem in Java");	
 	}
 }
->>>>>>> UseCase1-Employee_Attendance_Check
